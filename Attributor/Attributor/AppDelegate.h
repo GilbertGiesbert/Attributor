ab3d0520620c04d925d0762827dc2e021cac98a2
joern <Gerrit@kaiMac.local>
2016-03-02 11:15:20 +0100

    blank project

---
+++ Attributor/Attributor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Attributor
+//
+//  Created by iOS Entwickler on 02.03.16.
+//  Copyright (c) 2016 noorg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Attributor/Attributor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Attributor
+//
+//  Created by iOS Entwickler on 02.03.16.
+//  Copyright (c) 2016 noorg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
