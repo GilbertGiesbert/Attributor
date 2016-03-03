2c8cdf03995c5594884d540696c904452f96c731
joern <Gerrit@kaiMac.local>
2016-03-03 13:06:28 +0100

    - added buttons and labels stage 1

--- Attributor/Attributor/AttributorViewController.m
+++ Attributor/Attributor/AttributorViewController.m
@@ -9,10 +9,32 @@
 #import "AttributorViewController.h"
 
 @interface AttributorViewController ()
+@property (weak, nonatomic) IBOutlet UITextView *body;
+@property (weak, nonatomic) IBOutlet UILabel *headline;
 
 @end
 
 @implementation AttributorViewController
+
+
+- (IBAction)changeBodySelectionColorToMatchBackgroundColorOfButton:(UIButton *)sender {
+    
+    [self.body.textStorage addAttribute:NSForegroundColorAttributeName value:sender.backgroundColor range:self.body.selectedRange];
+}
+
+- (IBAction)outlineBodySelection {
+    
+    [self.body.textStorage addAttributes:@{ NSStrokeWidthAttributeName : @-3,
+                                           NSStrokeColorAttributeName : [UIColor blackColor]} range:self.body.selectedRange];
+}
+- (IBAction)unoutlineBodySelection {
+    
+    [self.body.textStorage removeAttribute:NSStrokeWidthAttributeName range:self.body.selectedRange];
+}
+
+- (void)viewDidLoad{
+    [super viewDidLoad];
+}
 
 
 
